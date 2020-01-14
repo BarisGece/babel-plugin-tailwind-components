c84b555867835ea7454a37e8b8cf713bb1b941fd
Barış Gece <gece.baris@gmail.com>
2020-01-14 17:22:17 +0300

    update - new dynamic styles added

--- src/dynamicStyles.js
+++ src/dynamicStyles.js
@@ -1,25 +1,26 @@
 let dynamicStyles = {
-  // https://tailwindcss.com/docs/background-color
-  // https://tailwindcss.com/docs/background-size
+  // backgroundColor.js - https://tailwindcss.com/docs/background-color
+  // backgroundPosition.js - https://tailwindcss.com/docs/background-position/
+  // backgroundSize.js - https://tailwindcss.com/docs/background-size
   bg: [
     { prop: 'backgroundColor', config: 'backgroundColor' },
-    { prop: 'backgroundSize', config: 'backgroundSize' },
-    { prop: 'backgroundPosition', config: 'backgroundPosition' }
+    { prop: 'backgroundPosition', config: 'backgroundPosition' },
+    { prop: 'backgroundSize', config: 'backgroundSize' }
   ],
 
-  // https://tailwindcss.com/docs/border-width
+  // borderColor.js - https://tailwindcss.com/docs/border-color
+  border: [
+    { prop: 'borderColor', config: 'borderColor' },
+    { prop: 'borderWidth', config: 'borderWidth' }
+  ],
+
+  // borderWidth.js - https://tailwindcss.com/docs/border-width
   'border-t': { prop: 'borderTopWidth', config: 'borderWidth' },
   'border-b': { prop: 'borderBottomWidth', config: 'borderWidth' },
   'border-l': { prop: 'borderLeftWidth', config: 'borderWidth' },
   'border-r': { prop: 'borderRightWidth', config: 'borderWidth' },
 
-  // https://tailwindcss.com/docs/border-color
-  border: [
-    { prop: 'borderWidth', config: 'borderWidth' },
-    { prop: 'borderColor', config: 'borderColor' }
-  ],
-
-  // https://tailwindcss.com/docs/border-radius
+  // borderRadius.js - https://tailwindcss.com/docs/border-radius
   'rounded-tl': { prop: 'borderTopLeftRadius', config: 'borderRadius' },
   'rounded-tr': { prop: 'borderTopRightRadius', config: 'borderRadius' },
   'rounded-br': { prop: 'borderBottomRightRadius', config: 'borderRadius' },
@@ -42,110 +43,129 @@ let dynamicStyles = {
   },
   rounded: { prop: 'borderRadius', config: 'borderRadius' },
 
-  // https://tailwindcss.com/docs/opacity
-  opacity: { prop: 'opacity', config: 'opacity' },
-
-  // https://tailwindcss.com/docs/shadows
+  // boxShadow.js - https://tailwindcss.com/docs/box-shadow/
   shadow: { prop: 'boxShadow', config: 'boxShadow' },
 
-  // https://tailwindcss.com/docs/width
-  w: { prop: 'width', config: 'width' },
-
-  // https://tailwindcss.com/docs/min-width
-  'min-w': { prop: 'minWidth', config: 'minWidth' },
-
-  // https://tailwindcss.com/docs/max-width
-  'max-w': { prop: 'maxWidth', config: 'maxWidth' },
-
-  // https://tailwindcss.com/docs/height
-  h: { prop: 'height', config: 'height' },
-
-  // https://tailwindcss.com/docs/min-height
-  'min-h': { prop: 'minHeight', config: 'minHeight' },
-
-  // https://tailwindcss.com/docs/max-height
-  'max-h': { prop: 'maxHeight', config: 'maxHeight' },
-
-  // https://tailwindcss.com/docs/spacing
-  pt: { prop: 'paddingTop', config: 'padding' },
-  pr: { prop: 'paddingRight', config: 'padding' },
-  pb: { prop: 'paddingBottom', config: 'padding' },
-  pl: { prop: 'paddingLeft', config: 'padding' },
-  px: { prop: ['paddingLeft', 'paddingRight'], config: 'padding' },
-  py: { prop: ['paddingTop', 'paddingBottom'], config: 'padding' },
-  p: { prop: 'padding', config: 'padding' },
-
-  mt: { prop: 'marginTop', config: 'margin' },
-  mr: { prop: 'marginRight', config: 'margin' },
-  mb: { prop: 'marginBottom', config: 'margin' },
-  ml: { prop: 'marginLeft', config: 'margin' },
-  mx: { prop: ['marginLeft', 'marginRight'], config: 'margin' },
-  my: { prop: ['marginTop', 'marginBottom'], config: 'margin' },
-  m: { prop: 'margin', config: 'margin' },
-
-  '-mt': { prop: 'marginTop', config: 'margin' },
-  '-mr': { prop: 'marginRight', config: 'margin' },
-  '-mb': { prop: 'marginBottom', config: 'margin' },
-  '-ml': { prop: 'marginLeft', config: 'margin' },
-  '-mx': { prop: ['marginLeft', 'marginRight'], config: 'margin' },
-  '-my': { prop: ['marginTop', 'marginBottom'], config: 'margin' },
-  '-m': { prop: 'margin', config: 'margin' },
-
-  // https://tailwindcss.com/docs/order
-  order: { prop: 'order', config: 'order' },
-
-  // https://tailwindcss.com/docs/svg
-  fill: { prop: 'fill', config: 'fill' },
-  stroke: { prop: 'stroke', config: 'stroke' },
-
-  // https://tailwindcss.com/docs/fonts
-  font: [
-    { prop: 'fontWeight', config: 'fontWeight' },
-    {
-      prop: 'fontFamily',
-      config: 'fontFamily'
-    }
-  ],
-  text: [
-    { prop: 'color', config: 'textColor' },
-    { prop: 'fontSize', config: 'fontSize' }
-  ],
-
-  // https://tailwindcss.com/docs/line-height
-  leading: { prop: 'lineHeight', config: 'lineHeight' },
-
-  // https://tailwindcss.com/docs/letter-spacing
-  tracking: { prop: 'letterSpacing', config: 'letterSpacing' },
-
-  // https://tailwindcss.com/docs/z-index
-  z: { prop: 'zIndex', config: 'zIndex' },
-  '-z': { prop: 'zIndex', config: 'zIndex' },
-
+  // cursor.js https://tailwindcss.com/docs/cursor/
   cursor: { prop: 'cursor', config: 'cursor' },
 
-  object: { prop: 'objectPosition', config: 'objectPosition' },
+  // fill.js - https://tailwindcss.com/docs/fill/
+  fill: { prop: 'fill', config: 'fill' },
 
+  // flex.js - https://tailwindcss.com/docs/flex/
   flex: { prop: 'flex', config: 'flex' },
+
+  // flexGrow.js - https://tailwindcss.com/docs/flex-grow/
   'flex-grow': { prop: 'flexGrow', config: 'flexGrow' },
+
+  // flexShrink.js - https://tailwindcss.com/docs/flex-shrink/
   'flex-shrink': { prop: 'flexShrink', config: 'flexShrink' },
 
-  list: { prop: 'listStyleType', config: 'listStyleType' },
+  // fontFamily.js - https://tailwindcss.com/docs/font-family/
+  // fontSize.js - https://tailwindcss.com/docs/font-size/
+  // fontWeight.js - https://tailwindcss.com/docs/font-weight/
+  // textColor.js - No Documentation Link
+  font: [
+    { prop: 'fontFamily', config: 'fontFamily' },
+    { prop: 'fontWeight', config: 'fontWeight' }
+  ],
+  text: [
+    { prop: 'fontSize', config: 'fontSize' },
+    { prop: 'color', config: 'textColor' }
+  ],
 
+  // height.js - https://tailwindcss.com/docs/height
+  h: { prop: 'height', config: 'height' },
+
+  // inset.js - https://tailwindcss.com/docs/top-right-bottom-left/
+  inset: { prop: ['top', 'right', 'bottom', 'left'], config: 'inset' },
+  'inset-y': { prop: ['top', 'bottom'], config: 'inset' },
+  'inset-x': { prop: ['left', 'right'], config: 'inset' },
   top: { prop: 'top', config: 'inset' },
   right: { prop: 'right', config: 'inset' },
   bottom: { prop: 'bottom', config: 'inset' },
   left: { prop: 'left', config: 'inset' },
+  '-inset': { prop: ['top', 'right', 'bottom', 'left'], config: 'inset' },
+  '-inset-y': { prop: ['top', 'bottom'], config: 'inset' },
+  '-inset-x': { prop: ['left', 'right'], config: 'inset' },
   '-top': { prop: 'top', config: 'inset' },
   '-right': { prop: 'right', config: 'inset' },
   '-bottom': { prop: 'bottom', config: 'inset' },
   '-left': { prop: 'left', config: 'inset' },
 
-  'inset-x': { prop: ['left', 'right'], config: 'inset' },
-  'inset-y': { prop: ['top', 'bottom'], config: 'inset' },
-  inset: { prop: ['top', 'right', 'bottom', 'left'], config: 'inset' },
-  '-inset-x': { prop: ['left', 'right'], config: 'inset' },
-  '-inset-y': { prop: ['top', 'bottom'], config: 'inset' },
-  '-inset': { prop: ['top', 'right', 'bottom', 'left'], config: 'inset' }
+  // letterSpacing.js - https://tailwindcss.com/docs/letter-spacing
+  tracking: { prop: 'letterSpacing', config: 'letterSpacing' },
+  '-tracking': { prop: 'letterSpacing', config: 'letterSpacing' },
+
+  // lineHeight.js - https://tailwindcss.com/docs/line-height
+  leading: { prop: 'lineHeight', config: 'lineHeight' },
+
+  // listStyleType.js - https://tailwindcss.com/docs/list-style-type
+  list: { prop: 'listStyleType', config: 'listStyleType' },
+
+  // margin.js - https://tailwindcss.com/docs/margin/
+  m: { prop: 'margin', config: 'margin' },
+  my: { prop: ['marginTop', 'marginBottom'], config: 'margin' },
+  mx: { prop: ['marginLeft', 'marginRight'], config: 'margin' },
+  mt: { prop: 'marginTop', config: 'margin' },
+  mr: { prop: 'marginRight', config: 'margin' },
+  mb: { prop: 'marginBottom', config: 'margin' },
+  ml: { prop: 'marginLeft', config: 'margin' },
+  '-m': { prop: 'margin', config: 'margin' },
+  '-my': { prop: ['marginTop', 'marginBottom'], config: 'margin' },
+  '-mx': { prop: ['marginLeft', 'marginRight'], config: 'margin' },
+  '-mt': { prop: 'marginTop', config: 'margin' },
+  '-mr': { prop: 'marginRight', config: 'margin' },
+  '-mb': { prop: 'marginBottom', config: 'margin' },
+  '-ml': { prop: 'marginLeft', config: 'margin' },
+
+  // maxHeight.js - https://tailwindcss.com/docs/max-height
+  'max-h': { prop: 'maxHeight', config: 'maxHeight' },
+
+  // maxWidth.js - https://tailwindcss.com/docs/max-width
+  'max-w': { prop: 'maxWidth', config: 'maxWidth' },
+
+  // minHeight.js - https://tailwindcss.com/docs/min-height
+  'min-h': { prop: 'minHeight', config: 'minHeight' },
+
+  // minWidth.js - https://tailwindcss.com/docs/min-width
+  'min-w': { prop: 'minWidth', config: 'minWidth' },
+
+  // objectPosition.js - https://tailwindcss.com/docs/object-position/
+  object: { prop: 'objectPosition', config: 'objectPosition' },
+
+  // opacity.js - https://tailwindcss.com/docs/opacity
+  opacity: { prop: 'opacity', config: 'opacity' },
+
+  // order.js - https://tailwindcss.com/docs/order
+  order: { prop: 'order', config: 'order' },
+
+  // padding.js https://tailwindcss.com/docs/padding/
+  p: { prop: 'padding', config: 'padding' },
+  py: { prop: ['paddingTop', 'paddingBottom'], config: 'padding' },
+  px: { prop: ['paddingLeft', 'paddingRight'], config: 'padding' },
+  pt: { prop: 'paddingTop', config: 'padding' },
+  pr: { prop: 'paddingRight', config: 'padding' },
+  pb: { prop: 'paddingBottom', config: 'padding' },
+  pl: { prop: 'paddingLeft', config: 'padding' },
+
+  // placeholderColor.js - https://tailwindcss.com/docs/placeholder-color/
+  // placeholderColor utility added
+  placeholder: { prop: 'color', config: 'placeholderColor' },
+
+  // stroke.js - https://tailwindcss.com/docs/stroke
+  // strokeWidth utility added
+  stroke: [
+    { prop: 'stroke', config: 'stroke' },
+    { prop: 'strokeWidth', config: 'strokeWidth' }
+  ],
+
+  // width.js - https://tailwindcss.com/docs/width
+  w: { prop: 'width', config: 'width' },
+
+  // zIndex.js - https://tailwindcss.com/docs/z-index
+  z: { prop: 'zIndex', config: 'zIndex' },
+  '-z': { prop: 'zIndex', config: 'zIndex' }
 }
 
 export default dynamicStyles
