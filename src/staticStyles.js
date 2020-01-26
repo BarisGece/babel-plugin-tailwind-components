a664acc030d74146cbe7a3de2b6f1245cf652edf
Barış Gece <gece.baris@gmail.com>
2020-01-27 01:54:15 +0300

    update - Composable transform utilities added

--- src/staticStyles.js
+++ src/staticStyles.js
@@ -1,4 +1,26 @@
 let staticStyles = {
+  // Composable transform utilities added - https://github.com/tailwindcss/tailwindcss/pull/1272
+  // transform.js
+  transform: {
+    '--transform-translate-x': '0',
+    '--transform-translate-y': '0',
+    '--transform-rotate': '0',
+    '--transform-skew-x': '0',
+    '--transform-skew-y': '0',
+    '--transform-scale-x': '1',
+    '--transform-scale-y': '1',
+    transform: [
+      'translateX(var(--transform-translate-x))',
+      'translateY(var(--transform-translate-y))',
+      'rotate(var(--transform-rotate))',
+      'skewX(var(--transform-skew-x))',
+      'skewY(var(--transform-skew-y))',
+      'scaleX(var(--transform-scale-x))',
+      'scaleY(var(--transform-scale-y))'
+    ].join(' ')
+  },
+  'transform-none': { transform: 'none' },
+
   // accessibility.js - https://tailwindcss.com/docs/screen-readers
   // Screen readers utilities added
   'sr-only': {
