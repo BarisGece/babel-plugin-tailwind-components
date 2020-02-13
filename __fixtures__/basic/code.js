8dd64932ccf94edf665d26312911fbfee42c0351
Barış Gece <gece.baris@gmail.com>
2020-02-14 01:28:43 +0300

    Update - Test added for GridRow&Column Gap

--- __fixtures__/basic/code.js
+++ __fixtures__/basic/code.js
@@ -5,3 +5,5 @@ let styles = tw`uppercase`
 let Box = tw.div`uppercase`
 
 let BoxExtended = tw(Box)`flex`
+
+let ColRowGap = tw` row-gap-4 col-gap-4`
--- __fixtures__/basic/output.js
+++ __fixtures__/basic/output.js
@@ -1,3 +1,8 @@
+import _tailwindUtils from 'tailwind-canary.macro/utils.umd.js'
+import _tailwindConfig2 from './tailwind.config.js'
+
+const _tailwindConfig = _tailwindUtils.resolveConfig(_tailwindConfig2)
+
 import _styled from '@emotion/styled'
 let styles = {
   textTransform: 'uppercase'
@@ -10,3 +15,10 @@ let Box = _styled.div({
 let BoxExtended = _styled(Box)({
   display: 'flex'
 })
+
+let ColRowGap = {
+  gridRowGap: _tailwindConfig.theme.gap['4'],
+  rowGap: _tailwindConfig.theme.gap['4'],
+  gridColumnGap: _tailwindConfig.theme.gap['4'],
+  columnGap: _tailwindConfig.theme.gap['4']
+}
